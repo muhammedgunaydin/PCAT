da3afc55cf499433bb543da233a6764e93bda221
Muhammed <muhammed-gunaydin@hotmail.com>
2022-03-31 23:11:24 +0300

    initial commit

--- app.js
+++ app.js
@@ -1,24 +1,79 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const fileUpload = require('express-fileupload');
 const ejs = require('ejs')
 const path = require('path')
+const Photo = require('./models/Photo')
+const fs = require('fs')
 
 
 const app = express()
+
+mongoose.connect('mongodb://127.0.0.1/pcat-test-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
 
 app.set("view engine", "ejs")
 
 
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended:true }))
+app.use(express.json())
+app.use(fileUpload());
 
-app.get('/', (req, res) => {
-    res.render('index')
+
+
+app.get('/', async (req, res) => {
+    const photos = await Photo.find({}).sort('-dateCreated')
+    res.render('index', {
+        photos,
+    })
 })
+
+app.get('/photos/:id', async (req, res) => {
+    //console.log(req.params.id)
+    // res.render('about')
+    const photo = await Photo.findById(req.params.id)
+    res.render('photo', {
+        photo
+    })
+
+})
+
 app.get('/about', (req, res) => {
     res.render('about')
 })
 app.get('/add', (req, res) => {
     res.render('add')
 })
+
+app.post('/photos', async (req, res) => {
+
+    // console.log(req.files.image)
+    // await Photo.create(req.body)
+    // res.redirect('/')
+
+    const uploadDir = 'public/uploads'
+
+    if (!fs.existsSync(uploadDir)) {
+        fs.mkdirSync(uploadDir)
+    }
+
+    let uploadeImage = req.files.image;
+    let uploadPath = __dirname + '/public/uploads/' + uploadeImage.name;
+
+    uploadeImage.mv(uploadPath, async () => {
+        await Photo.create({
+            ...req.body,
+            image: '/uploads/' + uploadeImage.name,
+        });
+        res.redirect('/')
+    })
+
+
+})
+
 
 const port = 3000
 app.listen(port, () => {

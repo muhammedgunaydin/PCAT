ac368e72ed9f3b69eeb8d15df8905f3a59bbc76a
Muhammed <muhammed-gunaydin@hotmail.com>
2022-04-06 23:07:56 +0300

    photo update change added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override')
 const ejs = require('ejs')
 const path = require('path')
 const Photo = require('./models/Photo')
@@ -18,9 +19,10 @@ app.set("view engine", "ejs")
 
 
 app.use(express.static('public'))
-app.use(express.urlencoded({ extended:true }))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(fileUpload());
+app.use(methodOverride('_method'))
 
 
 
@@ -71,9 +73,23 @@ app.post('/photos', async (req, res) => {
         res.redirect('/')
     })
 
-
 })
 
+app.get('/photos/edit/:id', async (req, res) => {
+    const photo = await Photo.findOne({ _id: req.params.id })
+    res.render('edit', {
+        photo
+    })
+})
+
+app.put('/photos/:id', async (req, res) => {
+    const photo = await Photo.findOne({ _id: req.params.id })
+    photo.title = req.body.title
+    photo.description = req.body.description
+    photo.save()
+
+    res.redirect(`/photos/${req.params.id}`)
+})
 
 const port = 3000
 app.listen(port, () => {
